3b5e719dcd160fbade8cdb1437a4cc60c0f7eeb2
nwpgm82 <66294174+nwpgm82@users.noreply.github.com>
2022-04-05 23:39:58 +0700

    update validate form (done)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="navbar">
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link :to="{ name: 'Home' }">Pokemon</router-link>
+    <router-link :to="{ name: 'Home' }">Function 1</router-link>
+    <router-link :to="{ name: 'Home' }">Function 2</router-link>
+    <router-link :to="{ name: 'Home' }">Function 3</router-link>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.navbar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 24px;
+    width: 100%;
+    margin: 50px 0;
+
+    a {
+        font-size: 24px;
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration: none;
+    }
+}
+</style>
